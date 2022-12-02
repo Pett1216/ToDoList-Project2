9f130f1626ce61652b4bc4eade86e525aa2e5c6a
petricie <117648722+Pett1216@users.noreply.github.com>
2022-12-02 22:05:22 +0100

    changes
    on html, css and js

---
+++ todolist.js
@@ -0,0 +1,11 @@
+/* function addTodo */
+
+    let input = document.getElementById (formName);
+    let tarea = document.getElementById (formDiscription);
+    let addBtn = document.getElementsByClassName (Addlist)
+
+
+    console.log("input : ", input.value);
+    console.log("tarea : ", tarea.value);
+
+
