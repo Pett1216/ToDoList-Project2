1a23693b308d14038e5d2ecfb15ae67d51e56199
petricie <117648722+Pett1216@users.noreply.github.com>
2022-12-03 13:05:37 +0100

    js added
    Some elements missing

--- todolist.js
+++ todolist.js
@@ -1,11 +1,28 @@
-/* function addTodo */
-
+function addTodo()
+{
     let input = document.getElementById (formName);
     let tarea = document.getElementById (formDiscription);
-    let addBtn = document.getElementsByClassName (Addlist)
+    let listE1 = addTodoElement(input.value);
+    let addTodo = document.getElementById (form-list);
 
+    addTodo.append (listE1);
 
     console.log("input : ", input.value);
     console.log("tarea : ", tarea.value);
+}
 
+function addTodoElement( value )
+{
+    let h4 = document.createElement('h4');
+    let p = document.createElement('p');
 
+    h4.innerText = "Name";
+    p.innerHTML = "<span>Discription<span/>";
+
+    let li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.append(h4);
+    li.append(p);
+
+    return li;
+}
